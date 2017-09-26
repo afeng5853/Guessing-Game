1bef307f239d1d5577e4a55b65441b30f2ccb6dc
BT_1N3_15 <BT_1N3_15@BT_1N3_15.bktech.local>
2017-09-26 12:58:22 -0400

    added comments

--- Guessing Game/src/GuessingGame.java
+++ Guessing Game/src/GuessingGame.java
@@ -8,9 +8,12 @@ import java.util.Scanner;
 
 public class GuessingGame {
 	public static void main(String[] args) {
+		//Random number from 1 to 10 to be guessed
 		int randNum = (int) (Math.random() * 10 + 1);
+		
 		Scanner input = new Scanner(System.in);
 		while (true) {
+		//Will break when the number is guessed correctly
 			System.out.print("Guess a number from 1-10: ");
 			try {
 				int guess = Integer.parseInt(input.nextLine());
@@ -19,6 +22,7 @@ public class GuessingGame {
 					break;
 				}
 			} catch (NumberFormatException e) {
+				//Occurs when anything but a number is read in
 				System.out.println("Error type: " + e);
 				System.out.println("Please enter a valid number");
 			}
